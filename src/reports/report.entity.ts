83e3a67d52f47bd2d1164a5cb009782c3fdb41a2
Yannick <yannickzahinda442@gmail.com>
2024-10-15 12:19:18 +0200

    add relationship user to report

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "src/users/user.entity";
+
 
 @Entity()
 export class Report {
@@ -25,4 +27,8 @@ export class Report {
 
     @Column()
     mileage: number;
+
+    @ManyToOne(() => User, (user) => user.reports)
+    user: User;
+
 }
