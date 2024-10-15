06b297cb6f9737311bdba0ac6fe676f7426b4e94
Yannick <yannickzahinda442@gmail.com>
2024-10-15 10:57:08 +0200

    add config service for env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { Database } from 'sqlite3';
 
 @Module({
   imports: [
@@ -14,13 +15,26 @@ import { Report } from './reports/report.entity';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true
-  }
-  ),UsersModule, 
+  //   TypeOrmModule.forRoot({
+  //   type: 'sqlite',
+  //   database: 'db.sqlite',
+  //   entities: [User, Report],
+  //   synchronize: true
+  // }
+  // )
+  TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => {
+      return {
+        type: 'sqlite',
+        database: config.get<string>('DB_NAME'),
+        synchronize: true,
+        entities: [User, Report],
+      };
+    }
+  })
+  ,
+  UsersModule, 
   ReportsModule
   ],
   controllers: [AppController],
